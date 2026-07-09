bec5b60738f7a8e0816f14500dcef84cf2738056
Nilamjr <nilamrj07@gmail.com>
2026-07-09 19:40:47 +1000

    Adding Countdigit program

---
+++ script.js
@@ -0,0 +1,44 @@
+//Write a function that return second largest number in an array
+// Corner cases 
+// what if array is empty
+// what if array has duplictes
+// what if array has negative numbers
+
+
+
+// let arr = [2, 6, 9, 10, 40, 30, 22, 28, 17, 18];
+
+
+// let arr = [2, 1, 9, 10];
+let arr = [2, 2, 2];
+
+let largest = -Infinity;
+let secondLargest;
+
+function searchLargest(arr) {
+    if (arr.length < 2) {
+        return "Array should have at least two numbers";
+    }
+
+
+    for (let i = 0; i < arr.length; i++) {
+
+        if (arr[i] > largest) {
+            secondLargest = largest;
+            largest = arr[i];
+        } else if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+
+    }
+    
+    if (secondLargest == -Infinity){
+        return "No second largest found";
+    }
+    return secondLargest;
+}
+
+let result = searchLargest(arr)
+console.log(result);
+// console.log("Largest value", largest);
+// console.log("Second Largest value", secondLargest);
---
+++ starPatterns.js
@@ -0,0 +1,127 @@
+// Star patterns
+
+// for (let i = 0; i < 2; i++) {
+//     let row = " "
+//     for (let j = 0; j < 4; j++) {
+//         row = row + "*";
+//     }
+//     console.log(row);
+// }
+
+
+// for (let i = 1; i <= 4; i++) {
+//     console.log("* ".repeat(i));
+// }
+
+for (let i = 0; i < 4; i++) {
+    let row = "";
+    for (let j = 0; j < i + 1; j++) {
+        row = row + "*";
+    }
+    // console.log(row);
+}
+
+let n = 4;
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j <= i; j++) {
+        row = row + (j + 1);
+    }
+    // console.log(row);
+}
+
+// let k = 5;
+// for (let i = 0; i < k; i++) {
+//     let row = "";
+//     for (let j = 0; j < i + 1; j++) {
+//         row = row + (i + 1);
+//     }
+//     // console.log(row);
+// }
+
+
+let p = 7;
+for (let i = 0; i < p; i++) {
+    let row = "";
+    for (let j = 0; j < p - i; j++) {
+        row = row + (j + 1);
+    }
+    // console.log(row);
+}
+
+for (let i = 0; i < 5; i++) {
+    let row = "";
+    for (let j = 0; j < 5 - i; j++) {
+        row = row + "*";
+    }
+    // console.log(row);
+}
+
+for (let i = 0; i < 5; i++) {
+    let row = "";
+    for (let j = 0; j < 5 - (i + 1); j++) {
+        row = row + " ";
+    }
+    for (let k = 0; k < i + 1; k++) {
+        row = row + "*";
+    }
+    console.log(row);
+}
+
+
+
+// 1
+// 1 0
+// 1 0 1
+// 1 0 1 0
+
+
+// for (let i = 0; i < n; i++){
+
+//     let row = "";
+//     let switchVar = 1;
+
+//     for(let j = 0; j < i + 1; j++){
+
+//         row = row + switchVar;
+
+//         if (switchVar == 1) {
+//             switchVar = 0;
+//         } else {
+//             switchVar = 1;
+//         }
+//     }
+
+//     // console.log(row);
+
+// }
+
+
+// New Pattern
+
+// 1
+
+// 0 1
+
+// 0 1 0
+
+// 1 0 1 0
+
+// 1 0 1 0 1
+
+
+let n = 10;
+
+let switchVar = 1;
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < i + 1; j++) {
+        row = row + switchVar;
+        if (switchVar == 1) {
+            switchVar = 0;
+        } else {
+            switchVar = 1;
+        }
+    }
+    console.log(row);
+}
