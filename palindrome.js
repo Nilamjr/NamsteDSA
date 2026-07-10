5d4d1ad62f0b9c6e08ec7b8dd900382af93f6189
Nilamjr <nilamrj07@gmail.com>
2026-07-10 17:30:37 +1000

    Palindrome Program

---
+++ palindrome.js
@@ -0,0 +1,26 @@
+function CheckPalindrome(x){
+
+    if(x < 0){
+        return false;
+    }
+
+    let xCopy = x;
+    let rev = 0;
+    while (x > 0) {
+        let rem = x % 10;
+        rev = (10*rev) + rem;
+        x = Math.floor(x / 10);
+    }
+
+    // if (xCopy === rev) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+
+    return rev === xCopy;
+}
+
+let res = CheckPalindrome(1221);
+
+console.log(res);
