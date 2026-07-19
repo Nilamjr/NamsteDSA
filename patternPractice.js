565ff238c8dcafca7b49c0790f1fc1dc81e78a0c
Nilamjr <nilamrj07@gmail.com>
2026-07-19 19:40:28 +1000

    Practicing and understanding loop

--- patternPractice.js
+++ patternPractice.js
@@ -6,13 +6,30 @@
 //  * * *
 //* * * *
 
-let row = "";
-
 let n = 5;
 
+
 for (let i = 0; i < n; i++) {
+    let row = "";
     for (let j = 0; j < n - i; i++) {
+
         row = row + "*";
+        console.log(row + i);
+        // console.log("i=" + i + "j=" + j);
     }
-    console.log(row);
 }
+
+
+
+// i  j    n-i 
+
+// 0  0  <  5-0=5    * * * * *
+//    1  <  5        
+//    2  < 5
+//    3  < 5
+//    4  < 5
+
+//    1  0  < 5-1 = 4
+//       1  < 4
+//       2 < 4
+//       3 < 4 
