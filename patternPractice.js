f0b113685282eac37c1ab20f2a2ffb6ae972a7bd
Nilamjr <nilamrj07@gmail.com>
2026-07-27 19:33:36 +1000

    Pattern with Digit

--- patternPractice.js
+++ patternPractice.js
@@ -1,21 +1,46 @@
 // pattern.js
 
 
+//      1
+//     12
+//    123
+//   1234
+//  12345
+// 123456
+
+
+let n = 6;
+for(let i=0; i<n; i++){
+  let row = "";
+  for(let j=0; j<n-i-1; j++){
+    row = row + " " ;
+  }
+  for(let k=0; k<i+1; k++){
+    row = row + (k+1);
+  }
+  console.log(row);
+}
+
+// n=6
+// i space digit 
+// 0 5     1
+// 1 4     2
+
 
 // *
 // **
 // ***
 // ****
 
-n = 4;
+// n = 4;
 
-for(let i=0; i<n; i++){
-let row = "";
- for(let j=0;j<=i;j++){
-     row = row + "*";
-    }
-    console.log(row);
-}
+// for(let i=0; i<n; i++){
+// let row = "";
+//  for(let j=0;j<=i;j++){
+//      row = row + "*";
+//     }
+//     console.log(row);
+// }
 
 
 
