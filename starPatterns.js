81a63585d55d65b850d161d0958ead12b9abd767
Nilamjr <nilamrj07@gmail.com>
2026-08-10 22:51:18 +1000

    pattern

--- starPatterns.js
+++ starPatterns.js
@@ -16,13 +16,27 @@
 // 2         2      2
 // 1         3      1
 
+// let n = 4;
+// for(let i = 0; i<n; i++){
+//     let row = "";
+//     for(let j = 0; j < i; j++){
+//         row = row + " ";
+//     } 
+//     for(let k = 0; k < n-i; k++){
+//         row = row + "* "
+//     }
+
+//     // console.log(row);
+// }
+
+
 let n = 4;
 for(let i = 0; i<n; i++){
     let row = "";
-    for(let j = 0; j < i; j++){
+    for(let j = 0; j < n-i-1; j++){
         row = row + " ";
     } 
-    for(let k = 0; k < n-i; k++){
+    for(let k = 0; k < i; k++){
         row = row + "* "
     }
 
