1b08bb436276f4f08281d0bf0a4d57fe95741b6f
Nilamjr <nilamrj07@gmail.com>
2026-08-13 23:01:07 +1000

    problem statement

--- buyAndSellStocks.js
+++ buyAndSellStocks.js
@@ -1,3 +1,7 @@
+// Problem Statement:
+// You are given an array prices where prices[i] is the price of a given stock on the ith day. You want to maximize your profit 
+// by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
 // let arr = [7,6,4,3,1];
 // for(let i = 0; i < arr.length; i++){
@@ -5,29 +9,27 @@
 //     for(let j = i+1; j<arr.length; j++){
 //         console.log("i= ", arr[i] + "j= ", arr[j]); 
 //     }
-
 // }
 
-let arr = [11,3,7,3,5,7,6,1,4];
-let buyAndSellStock = function(arr){
+let prices = [11,3,7,3,5,7,6,1,4];
+let buyAndSellStock = function(prices){
 
-    let min = arr[0];
+    let min = prices[0];
     let maxProfit = 0;   
-    for(let i = 0; i < arr.length; i++){
+    for(let i = 1; i < prices.length; i++){
             
-        if(arr[i]-min > maxProfit){
-            maxProfit = arr[i]-min;
+        if(prices[i]-min > maxProfit){
+            maxProfit = prices[i]-min;
         }
-        
-        if(arr[i] < min){
-            min = arr[i];
+        if(prices[i] < min){
+            min = prices[i];
         }
-
     }
     return maxProfit;
   }
 
-  buyAndSellStock();
+  let result = buyAndSellStock(prices);
+  console.log(result);
 
 
 
