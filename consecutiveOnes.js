47b2986e1b45e0f870b73ce2270e16d80f5ec754
Nilamjr <nilamrj07@gmail.com>
2026-08-22 22:04:21 +1000

    ConsecutiveOnes Program

---
+++ consecutiveOnes.js
@@ -0,0 +1,22 @@
+
+// Binary array 
+[0,1,1,0,0,1,1,1,0,1,0,0,1,1,1,1,0,1,0]
+    2        3     1       4         1
+
+
+maxCount = 0;
+CurrentCount = 0
+
+loop - 
+if value = 1 {
+    CurrentCount++
+
+}
+
+else {
+    maxCount > CurrentCount -> maxCount ? CurrentCount
+    CurrentCount = 0;
+}
+
+
+
